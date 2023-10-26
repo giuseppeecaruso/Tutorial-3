dfc90c9f2afa20f34899742da252d5a4eaa83cab
giuseppeecaruso <146777662+giuseppeecaruso@users.noreply.github.com>
2023-10-26 14:16:04 +0100

    Update problem1.py

--- src/starter_code/problem1.py
+++ src/starter_code/problem1.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import pandas as pd
 
 def create_dataframe(csv_file):
     """ Creates, prints and returns a pandas dataframe containing data from a csv file
@@ -14,17 +14,18 @@ def create_dataframe(csv_file):
     # 1. Complete the code to create a variable called 'df' that is a pandas dataframe that reads its contents from the
     # csv file.
     # Use pandas.read_csv https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
-
+    df = pd.read_csv(csv_file)
     # Uncomment the 2 lines of code below to set the pandas display options to show all rows and columns of the dataset
     # Otherwise, by default pandas will only print as many columns and rows as can be displayed in your terminal window
-
-    # pd.set_option('display.max_rows', df.shape[0] + 1)
-    # pd.set_option('display.max_columns', df.shape[1] + 1)
+    pd.set_option('display.max_rows', df.shape[0] + 1)
+    pd.set_option('display.max_columns', df.shape[1] + 1)
+    
+    print(df)
 
     # 2. Add a line of code to using Python print() to print the dataframe contents
 
     # 4. Complete the line of code to return the dataframe
-    return
+    return df
 
 
 if __name__ == '__main__':
@@ -32,3 +33,4 @@ if __name__ == '__main__':
     raw_data_file = Path(__file__).parent.parent.joinpath('data', 'paralympics_raw.csv')
     # Call the create_dataframe function, passing the csv file as argument
     df_raw = create_dataframe(raw_data_file)
+    print(df_raw)
